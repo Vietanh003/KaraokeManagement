8ae2bc089370e0c81af6b58a486c00cfa8b3db2e
MinhTri196 <minhtri586521@gmail.com>
2025-05-28 02:11:36 +0700

    Add files via upload

---
+++ cpp/QL_Karaoke/QL_Karaoke/thongkedt.h
@@ -0,0 +1,10 @@
+﻿#ifndef THONGKEDT_H
+#define THONGKEDT_H
+
+#include "common.h"
+#include "hoadon.h"
+
+// Thống kê doanh thu theo khoảng thời gian
+double thongKeDoanhThu(Node* goc, struct tm bat_dau, struct tm ket_thuc);
+
+#endif
