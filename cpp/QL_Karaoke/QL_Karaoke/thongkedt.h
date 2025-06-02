522a19a7cca1e66dd790c2d98bd35ca99f203d86
VietAnh <vanhdz246@gmail.com>
2025-06-02 19:58:35 +0700

    upMinhTri

--- cpp/QL_Karaoke/QL_Karaoke/thongkedt.h
+++ cpp/QL_Karaoke/QL_Karaoke/thongkedt.h
@@ -4,7 +4,14 @@
 #include "common.h"
 #include "hoadon.h"
 
-// Thống kê doanh thu theo khoảng thời gian
 double thongKeDoanhThu(Node* goc, struct tm bat_dau, struct tm ket_thuc);
+
+double thongKeDoanhThuTheoNgay(Node* goc, int year, int month, int day);
+
+double thongKeDoanhThuTheoThang(Node* goc, int year, int month);
+
+double thongKeDoanhThuTheoQuy(Node* goc, int year, int quarter);
+
+double thongKeDoanhThuTheoNam(Node* goc, int year);
 
 #endif
